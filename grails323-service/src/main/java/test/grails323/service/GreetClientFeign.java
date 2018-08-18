4f6e5de0cc5a1d03f0fa52fa7d92281fc5eb6c07
Bin Le <bin.le.code@gmail.com>
2018-08-17 21:26:08 -0400

    externalize feign client's service name to application.yml

--- grails323-service/src/main/java/test/grails323/service/GreetClientFeign.java
+++ grails323-service/src/main/java/test/grails323/service/GreetClientFeign.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * need to add @EnableFeignClient to boot application class
  * also need to register singleton bean in conf/spring/resources.groovy
+ *
+ * the service name for the feign client can be external properties from application.yml
  */
-@FeignClient(name = "grails335-service", fallback = GreetClientFailover.class)
+//@FeignClient(name = "grails335-service", fallback = GreetClientFailover.class)
+@FeignClient(name = "${app.greetServiceName}", fallback = GreetClientFailover.class)
 public interface GreetClientFeign {
 
     @GetMapping("/greet")
